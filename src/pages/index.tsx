64677807519969393f850e0dee0342048ff8c813
Leonardo  Lopes <leonardolivelopes2@gmail.com>
2022-03-26 02:01:56 -0300

    layout inicial pagina home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { GetStaticProps } from 'next';
-
+import { BsCalendar4 } from 'react-icons/bs';
+import { AiOutlineUser } from 'react-icons/ai';
+import Link from 'next/link';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +26,40 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  return (
+    <div className={styles.container}>
+      <header>
+        <img src="/logo.svg" alt="logo" />
+      </header>
+      <section>
+        <article className={styles.postContainer}>
+          <Link href="/post">
+            <a>
+              <h2 className={styles.title}>Titulo da página</h2>
+              <strong>Subtitle do trem aqui mermão</strong>
+              <div>
+                <time>
+                  <BsCalendar4 />
+                  15 de mar 2021
+                </time>
+                <p>
+                  <AiOutlineUser />
+                  Autor do post
+                </p>
+              </div>
+            </a>
+          </Link>
+        </article>
+      </section>
+    </div>
+  );
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
-
-//   // TODO
-// };
+export const getStaticProps = async () => {
+  return {
+    props: {
+      posts: {},
+    },
+  };
+};
